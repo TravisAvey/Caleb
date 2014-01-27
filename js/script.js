df9305c583cbb617228277c9c78dafe19f1c6048
TravisAvey <avey.travis@gmail.com>
2014-01-26 22:31:25 -0600

    small fixes, removed uneccassary files

--- js/script.js
+++ js/script.js
@@ -16,15 +16,4 @@
 		return false;
 	});
 
-	$("#jquery_jplayer_1").jPlayer({
-        ready: function () {
-          $(this).jPlayer("setMedia", {
-            mp3: "media/test.mp3",
-            ogg: "media/test.ogg"
-          });
-        },
-        swfPath: "/js",
-        supplied: "mp3, ogg"
-      });
-
 })();
