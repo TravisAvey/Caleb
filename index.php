91c978850c1e7bc6202ce209134535c83d5e798c
TravisAvey <avey.travis@gmail.com>
2014-01-28 19:45:15 -0600

    added a media query. added styling for browsers with and without display flex

--- index.php
+++ index.php
@@ -29,6 +29,22 @@ include 'inc/header.php';
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
 	</article>
 
 	<div class="video">
@@ -42,11 +58,11 @@ include 'inc/header.php';
 		<p>Your browser doesn't support playing audio files</p>
 	</audio> -->
 
-		<div class="soundcloud">
-			<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/21487637&amp;color=ffae02&amp;auto_play=false&amp;show_artwork=true"></iframe>
-		</div>
-
 </section>
+<h3 class="music-title">Listen to some of Caleb's Music!</h3>
+<div class="soundcloud">
+	<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/21487637&amp;color=ffae02&amp;auto_play=false&amp;show_artwork=true"></iframe>
+</div>
 <?php 
 include 'inc/footer.php';
  ?>
