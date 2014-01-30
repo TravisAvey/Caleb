69240d117cd381eea2d4993b629872716837d381
TravisAvey <avey.travis@gmail.com>
2014-01-29 19:52:23 -0600

    Updated error redirect pages 404 and 500

--- error/notfound.php
+++ error/notfound.php
@@ -4,7 +4,10 @@ include '../inc/header.php';
  ?>
 
 <div class="error">
-	<h1> Error 404! </h1>
+	<h1>Error:</h1>
+	<div class="circle">
+		<h1> 404 </h1>
+	</div>
 	<h1>Sorry, the page you have requested was not found.</h1>
 	<p>Please check the URL and try again.</p>
 </div>
--- error/serverer.php
+++ error/serverer.php
@@ -3,8 +3,11 @@ include '../inc/header.php';
  ?>
 
 <div class="error">
-	<h1> Error 500! </h1>
-	<p>Something went horribly wrong.</p>
+	<h1>Error:</h1>
+	<div class="circle">
+		<h1> 500</h1>
+	</div>
+	<h1>Something went horribley wrong.</h1>
 </div>
 
  <?php 
