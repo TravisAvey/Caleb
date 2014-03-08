6d41c9e5ffd3838f039bf07245f95431c9310d4d
TravisAvey <avey.travis@gmail.com>
2014-03-08 10:56:35 -0600

    Updated error pages

--- error/notfound.php
+++ error/notfound.php
@@ -1,19 +1,17 @@
 <?php 
 include '../inc/header.php';
-
  ?>
-
 <div class="error">
-	<h1>Error:</h1>
 	<div class="circle">
 		<h1> 404 </h1>
 	</div>
-	<h1>Sorry, the page you have requested was not found.</h1>
-	<p>Please check the URL and try again.</p>
+	<div class="errorMessage">
+		<h1>Error:</h1>
+		<h1>Sorry, the page you have requested was not found.</h1>
+		<p>Please check the URL and try again.</p>
+		<p>Or go back <a href="/caleb_westbrook/">Home</a></p>
+	</div>
 </div>
-
 <?php 
-
 include '../inc/footer.php';
-
  ?>
--- error/serverer.php
+++ error/serverer.php
@@ -1,15 +1,16 @@
 <?php 
 include '../inc/header.php';
  ?>
-
 <div class="error">
-	<h1>Error:</h1>
 	<div class="circle">
-		<h1> 500</h1>
+		<h1> 500 </h1>
 	</div>
-	<h1>Something went horribley wrong.</h1>
+	<div class="errorMessage">
+		<h1>Error:</h1>
+		<h1>Sorry, something went horribley wrong</h1>
+		<p>Refresh or go back <a href="/caleb_westbrook/">Home</a></p>
+	</div>
 </div>
-
  <?php 
 include '../inc/footer.php';
   ?>
